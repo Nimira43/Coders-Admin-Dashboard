c501d9b5790d66407afcd2342ca9ca8f0fdf14b1
Nimira43 <n.rabone72@aol.com>
2024-10-13 21:43:17 +0100

    Setting up pages in App.js and the routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Stats from './pages/Stats'
+import Upgrades from './pages/Upgrades'
+import Posts from './pages/Posts'
+import Error from './pages/Error'
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <h1>Coders Admin Dashboard</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/stats' element={<Stats />}></Route>
+          <Route path='/upgrades' element={<Upgrades />}></Route>
+          <Route path='/posts' element={<Posts />}></Route>
+          <Route path='/error' element={<Error />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
-
-export default App
 
