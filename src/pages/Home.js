ba32a556f767a16f16598653a7c01fe0f460c4a6
Nimira43 <n.rabone72@aol.com>
2024-12-07 21:46:02 +0000

    Updated colour scheme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <>
-      <div className="section">
+      <div className='section'>
         <h1>Dashboard - Home</h1>
         <br />
-        <div className="cards">
+        <div className='cards'>
           {links.map((link) => (
             <Link 
               to={link.url} 
